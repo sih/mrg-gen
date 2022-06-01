b246b4fef2ba6e2b043198db18eed3dedff4c1b6
Sid Haniff <syed_haniff@hotmail.com>
2022-06-01 20:25:25 +0100

    Add some newly discovered test cases and a github build action

--- src/test/java/ltd/datasoc/labs/ctwg/mrg/MRGlossaryGeneratorTest.java
+++ src/test/java/ltd/datasoc/labs/ctwg/mrg/MRGlossaryGeneratorTest.java
@@ -2,9 +2,13 @@ package ltd.datasoc.labs.ctwg.mrg;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Many of the requirements are taken from
+ * https://essif-lab.github.io/framework/docs/tev2/tev2-toolbox#creating-an-mrg
+ *
  * @author sih
  */
 class MRGlossaryGeneratorTest {
@@ -15,4 +19,18 @@ class MRGlossaryGeneratorTest {
     assertThat(mrg).isNotNull();
   }
 
+  /*
+   *
+   */
+  @DisplayName(
+      "Given an MRG with an existing id; When generating from a term file with the same id; Then the existing glossary entry should be replaced by the newer")
+  @Test
+  void testMatchingIdsAreReplaced() {
+    // TODO impelment me
+  }
+
+  @DisplayName(
+      "Given more than one formphrases field; When generating an MRG; Then the formfields must be mutually exclusive wrt elements")
+  @Test
+  void testFormfieldsAreMutuallyExclusiveByElement() {}
 }
