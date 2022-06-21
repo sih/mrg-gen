97b64d08b85a7199b3f722b2ed734eca7e022430
Sid Haniff <syed_haniff@hotmail.com>
2022-06-21 13:53:02 +0100

    Add check for local when deriving ownerRepo and rootPathDir from the scopedir

--- src/main/java/ltd/datasoc/labs/ctwg/mrg/connectors/LocalFSConnector.java
+++ src/main/java/ltd/datasoc/labs/ctwg/mrg/connectors/LocalFSConnector.java
@@ -5,7 +5,9 @@ import static ltd.datasoc.labs.ctwg.mrg.processors.MRGGenerationException.COULD_
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import ltd.datasoc.labs.ctwg.mrg.processors.MRGGenerationException;
 
 /**
@@ -26,8 +28,34 @@ public class LocalFSConnector implements MRGConnector {
     return content;
   }
 
+  private String getContent(Path contentPath) throws MRGGenerationException {
+    String content;
+    try {
+      content = new String(Files.readAllBytes(contentPath));
+    } catch (IOException e) {
+      throw new MRGGenerationException(
+          String.format(COULD_NOT_READ_LOCAL_CONTENT, contentPath.toUri()));
+    }
+    return content;
+  }
+
   @Override
   public List<FileContent> getDirectoryContent(String repository, String directoryName) {
-    return null;
+    List<FileContent> contents;
+    Path directoryPath = Path.of(repository, directoryName);
+    try {
+      Stream<Path> contentsAsPath = Files.walk(directoryPath);
+      contents =
+          contentsAsPath
+              .map(
+                  path ->
+                      new FileContent(
+                          path.getFileName().toString(), this.getContent(path), new ArrayList<>()))
+              .toList();
+    } catch (IOException ioe) {
+      throw new MRGGenerationException(
+          String.format(COULD_NOT_READ_LOCAL_CONTENT, directoryPath.toUri()));
+    }
+    return contents;
   }
 }
